33690497e4cad838d0b1e869fa9f44e8a4fea79e
Arjun singh <79888586+Arjunsingh606@users.noreply.github.com>
2023-03-19 14:14:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let x;
+let m = 0;
+let time = m.localStorage.getItem("mytime");
+function start() {
+    x = setInterval(anim, 100);
+
+    function anim() {
+
+        console.log(m);
+        if (m == 1200) {
+            clearInterval(x);
+            m = 0;
+        }
+
+        else {
+            m = m + 5;
+         document.getElementById("im").style.visibility = "visibiliy";
+         let target = document.getElementById("im");   
+         target.style.marginLeft = m + "px";
+        }
+    }
+
+}
+
+function stop() {
+    clearInterval(x)
+}
